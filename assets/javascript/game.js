e18ebac5564629708382618699a86e8cf270c6fe
awaheed <awaheed@utexas.edu>
2019-07-01 15:50:40 -0500

    updated code html and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,8 +17,12 @@
 
 // https://stackoverflow.com/questions/28007949/how-to-convert-array-into-string-without-comma-and-separated-by-space-in-javascr/28007965
 
-// Initialize variables
-var count_guess = 15;
+//https://www.quora.com/How-do-you-play-audio-files-on-Javascript
+
+//https://www.quora.com/How-do-you-insert-an-image-in-Javascript
+
+// Initialize variables when page loads
+var count_guess = 10;
 var userguess = [];
 var answer = [];
 var blank_answer = [];
@@ -26,198 +30,193 @@ var x = 0;
 var win = 0;
 var loss = 0;
 
-// initialize computer guess on page referesh
-var country_lib = ["Pakistan", "India", "France", "Russia", "Italy", "Germany", "Mexico", "Canada"];
-var comp_guess = country_lib[Math.floor(Math.random() * country_lib.length)].toLowerCase();
-console.log(comp_guess);
-console.log(comp_guess.length);
+// initialize hint for country when page loads
+var hintPakistan = ["Second Tallest Mountain In The World", "Country Of Oldest Salt Mine", "Markhor in the National Animal", "Largest producer of handsewn soccer balls", "The oldest covilization exist in this country", "The country name translates to Land of the Pure"];
+var hintIndia = ["Largest democracies in the world", "Belongs to the continent of Asia", "Former British colony", "Largest tea producer", "World largest milk producer", "Has a seven wonder of the world"];
+var hintFrance = ["Notre-Dame", "Louvre Museum", "One of the four grand slam tennis eventsis held every year", "Has the highest number of Nobel Prizes for Literature", "Napoleon", "D-Day landings"];
+var hintRussia = ["russia1", "russia2", "russia3", "russia4", "russia5", "russia6"];
 
-// hint for country
-var hintPakistan=["pakistan1","pakistan2","pakistan3","pakistan4","pakistan5","pakistan6"];
-var hintIndia=["India1","India2","India3","India4","India5","India6"];
-var hintFrance=["france1","france2","france3","france4","france5","france6"];
-var hintRussia=["russia1","russia2","russia3","russia4","russia5","russia6"];
+// array object of country facts
 
-$("#startGame").on("click",function(){
-    alert("start");
-})
+var countryFacts = [{
+    flag: "./assets/images/pkFlag.png",
+    audio: "./assets/images/pk.mp3",
+    fact: "Pakistan is officially called the Islamic Republic of Pakistan. Pakistan became independent in from the British Indian Empire in 1947. The official currency of Pakistan is the Pakistani rupee. Cricket is the most popular sport in Pakistan "
+},
+{
+    flag: "./assets/images/india.png",
+    audio: "./assets/images/india.mp3",
+    fact: "India is officially known as the Republic of India. Pakistan became independent from the British Indian Empire on 15th August 1947 just one day after Pakistan. Once British rule was over, India was split into the Republic of India and the Islamic Republic of Pakistan. Later, an area between India and Pakistan became the People's Republic of Bangladesh. Many different languages are spoken in India. The main ones are Hindi, Bengali, Telugu, Marathi, Tamil, and Urdu. "
+},
 
-//loadHint array based on user guess
-if (comp_guess==="pakistan"){
-    var loadHint=hintPakistan;
-}
+{
+    flag: "./assets/images/france.png",
+    audio: "./assets/images/france.mp3",
+    fact: "France has 28 UNESCO World Heritage Sites. It is the largest country in the EU. ‘Liberty, equality and fraternity’ (or brotherhood) is the national motto of France. Europe’s highest mountain is in the French Alps. Eiffel Tower, Notre Dame, Louvre, Montmartre, Arc de Triomphe, the river Seine and many other great attractions. Normandy were the cite for D-day landings"
+},
 
-if (comp_guess==="india"){
-    var loadHint=hintIndia;
-}
-
-if (comp_guess==="russia"){
-    var loadHint=hintRussia;
-}
-
-if (comp_guess==="france"){
-    var loadHint=hintFrance;
-}
-
-// When the hint is clicked...
-$("#hintButton").on("click", function() {
-	// We generate a random number between 0 and length of hint array 
-	var number = Math.floor((Math.random() * loadHint.length));
-	// We display the fact from the loadHint that is in the random position we just generated.
-	$("#hintText").text(loadHint[number]);
-})
-
-//initialize __(blank) array with dashes __ 
-
-for (var i = 0; i < comp_guess.length; i++) {
-    blank_answer[i] = "_"
-}
-console.log(blank_answer);
-
-//print _ (blanks) in the country 
-
-document.getElementById("country").innerHTML = blank_answer.join(' ');
+];
 
 // Output inital counter variables on html
 document.getElementById("guess_left").innerHTML = count_guess;
-document.getElementById("loss").innerHTML = loss;
 
-document.onkeyup = function (event) {
+// initialize computer guess on clicking start button
 
-    //start game with a space bar (need to work)
+$("#startGame").on("click", function () {
 
-    var charCode = event.keyCode; //check ascii code for user entry
+    userguess = [];
+    answer = [];
+    blank_answer = [];
+    count_guess = 10
+    userguess = [];
+    x = 0;
 
-    //check condition if user enters letters only
+    document.getElementById("guess_left").innerHTML = count_guess;
+    document.getElementById("guess").innerHTML = userguess;
+    document.getElementById("hintText").innerHTML = "Click the button for hint";
+    document.getElementById("interstingFact").innerHTML = "";
+    document.getElementById("imageFlag").src = "./assets/images/Capture-flag.JPG";
+    document.getElementById("nationalAnthem").src = "";
 
-    if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || charCode == 8) {
 
-        //check condition if element already exist in array
+    var country_lib = ["Pakistan", "India", "France", "Russia"];
+    var comp_guess = country_lib[Math.floor(Math.random() * country_lib.length)].toLowerCase();
+    console.log(comp_guess);
+    console.log(comp_guess.length);
 
-        if (userguess.includes(String.fromCharCode(event.keyCode).toLowerCase()) === true) {
-            alert("letter already exist");
-        }
+    //initialize __(blank) array with dashes __ 
 
-        else {
+    for (var i = 0; i < comp_guess.length; i++) {
+        blank_answer[i] = "_"
+    }
+    console.log(blank_answer);
 
-            //if the element is "new" continue code line
+    //print _ (blanks) in the country 
 
-            userguess[x] = event.key; //log user guess in the array at index position x
-            console.log(userguess);
-            console.log(event.key);
-            document.getElementById("guess").innerHTML = userguess;
+    document.getElementById("country").innerHTML = blank_answer.join(' ');
 
-            // check if unable to guess
-
-            if (count_guess === 0) {
-                alert("Sorry!! You were unable to guess the country" + " " + comp_guess);
-                return;
-            }
-
-            count_guess--;
-            document.getElementById("guess_left").innerHTML = count_guess;
-
-            // store index position of the comp_guess word and user guess letter 
-            var indices = [];
-            for (var i = 0; i < comp_guess.length; i++) {
-                if (comp_guess[i] === userguess[x]) indices.push(i);
-            };
-
-            console.log(indices);
-
-            // store common user guess letter in the answer array
-
-            if (indices.length > 0) {
-                for (var j = 0; j < indices.length; j++) {
-                    blank_answer[indices[j]] = userguess[x];
-                    // debugger;
-                    console.log(blank_answer);
-                };
-            };
-
-            document.getElementById("country").innerHTML = blank_answer.join(' ');
-
-            // check for winning condition
-
-            if (blank_answer.indexOf("_") === -1) {
-                alert("Kuddos!! For guessing the country");
-            }
-            x++;
-        }
-
-        // debugger;
+    //loadHint array based on user guess
+    if (comp_guess === "pakistan") {
+        var loadHint = hintPakistan;
     };
-};
-// document.onkeyup=function(event){
 
-//     var charCode=event.keyCode;
+    if (comp_guess === "india") {
+        var loadHint = hintIndia;
+    }
 
-//     console.log(userguess);
+    if (comp_guess === "russia") {
+        var loadHint = hintRussia;
+    }
 
-//     //check condition if user enters letters only
+    if (comp_guess === "france") {
+        var loadHint = hintFrance;
+    }
 
-//     if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || charCode == 8){
+    // When the hint is clicked...
+    $("#hintButton").on("click", function () {
+        // We generate a random number between 0 and length of hint array 
+        var number = Math.floor((Math.random() * loadHint.length));
+        // We display the fact from the loadHint that is in the random position we just generated.
+        $("#hintText").text(loadHint[number]);
+    })
 
-//         //check condition if element already exist in array
+    document.onkeyup = function (event) {
 
-//         if(userguess.includes(String.fromCharCode(event.keyCode).toLowerCase())===true){
-//             alert("letter already exist");
-//         }
+        //check if stat button has been clicked
 
-//         else{
+        var charCode = event.keyCode; //check ascii code for user entry
 
-//             //if the element is "new" continue code line
+        //check condition if user enters letters only
 
-//             userguess[x]=String.fromCharCode(event.keyCode).toLowerCase();
+        if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || charCode == 8) {
 
-//             document.getElementById("guess").innerHTML=userguess;
+            //check condition if element already exist in array
 
-//             count_guess--;
-//             document.getElementById("guess_left").innerHTML=count_guess;
+            if (userguess.includes(String.fromCharCode(event.keyCode).toLowerCase()) === true) {
+                alert("letter already exist");
+            }
 
-//             // Check for win condition
+            else {
 
-//             if (userguess[x]===comp_choice){
-//                 alert("Kuddos!! on guessing the letter" +" "+comp_choice);
-//                 win++; 
-//                 document.getElementById("win").innerHTML=win;
-//                 userguess =[];
-//                 document.getElementById("guess").innerHTML=userguess;
-//                 count_guess=9;
-//                 document.getElementById("guess_left").innerHTML=count_guess;
-//                 x=0;
+                //if the element is "new" continue code line
 
-//                 // re-start with new computer guess
-//                 comp_choice=comp_letters[Math.floor(Math.random() * comp_letters.length)];
-//                 return;
-//             };
+                userguess[x] = event.key; //log user guess in the array at index position x
+                console.log(userguess);
+                console.log(event.key);
+                document.getElementById("guess").innerHTML = userguess;
 
-//             // Check if user is unable to guess
+                // check if unable to guess
 
-//             if (count_guess===0){
-//                 alert("Sorry!! You were unable to guess the letter"+" "+comp_choice);
-//                 loss++
-//                 document.getElementById("loss").innerHTML=loss;
-//                 userguess =[];
-//                 document.getElementById("guess").innerHTML=userguess;
-//                 count_guess=9;
-//                 document.getElementById("guess_left").innerHTML=count_guess;
-//                 x=0;
+                if (count_guess === 0) {
+                    alert("Sorry!! You were unable to guess the country" + " " + comp_guess);
+                    return;
+                }
 
-//                 // re-start with new computer guess
-//                 comp_choice=comp_letters[Math.floor(Math.random() * comp_letters.length)];
-//                 return;
-//             };   
+                count_guess--;
+                document.getElementById("guess_left").innerHTML = count_guess;
 
-//             x++;
-//         };
-//         console.log(comp_choice);
-//     }
+                // debugger;
 
-//     else{
-//         alert("Enter letter only");
-//     };
-//     // debugger;
-// };
+                // store index position of the comp_guess word and user guess letter 
+                var indices = [];
+                for (var i = 0; i < comp_guess.length; i++) {
+                    if (comp_guess[i] === userguess[x]) indices.push(i);
+                };
+
+                // debugger;
+                console.log(indices);
+
+                // store common user guess letter in the answer array
+
+                if (indices.length > 0) {
+                    for (var j = 0; j < indices.length; j++) {
+                        blank_answer[indices[j]] = userguess[x];
+                        // debugger;
+                        console.log(blank_answer);
+                    };
+                };
+
+                document.getElementById("country").innerHTML = blank_answer.join(' ');
+
+                // check for winning condition
+
+                if (blank_answer.indexOf("_") === -1) {
+                    alert("Kuddos!! For guessing the country");
+                    if (comp_guess === "pakistan") {
+
+                        document.getElementById("imageFlag").src = countryFacts[0].flag;
+                        document.getElementById("nationalAnthem").src = countryFacts[0].audio;
+                        document.getElementById("interstingFact").innerHTML = countryFacts[0].fact;
+
+                    };
+                    if (comp_guess === "india") {
+
+                        document.getElementById("imageFlag").src = countryFacts[1].flag;
+                        document.getElementById("nationalAnthem").src = countryFacts[1].audio;
+                        document.getElementById("interstingFact").innerHTML = countryFacts[1].fact;
+
+                    };
+                    if (comp_guess === "france") {
+
+                        document.getElementById("imageFlag").src = countryFacts[2].flag;
+                        document.getElementById("nationalAnthem").src = countryFacts[2].audio;
+                        document.getElementById("interstingFact").innerHTML = countryFacts[2].fact;
+
+                    };
+                }
+                x++;
+            }
+
+            // debugger;
+        };
+    };
+})
+
+
+
+
+
+// document.getElementById("loss").innerHTML = loss;
+
 
 
